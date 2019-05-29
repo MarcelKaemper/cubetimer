f8fee08b8d6150467ea380b2994f370f3b9fb50a
MarcelKaemper <marcelkaemper862@gmail.com>
2019-05-29 21:37:15 +0200

    Added arduino button listener

---
+++ cubetimer.ino
@@ -0,0 +1,44 @@
+#include <Eventually.h>
+
+#define BTN1 5
+#define BTN2 6
+#define BUZZ 7
+
+int status = 0;
+
+EvtManager mgr;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(BTN1, INPUT);
+  pinMode(BTN2, INPUT);
+
+  mgr.addListener(new EvtPinListener(BTN1, (EvtAction)down));
+}
+
+bool down(){
+   
+  if(digitalRead(BTN2) == HIGH){
+
+    status = !status;
+
+    if(status){
+      Serial.println("tmr_start");  
+    }else{
+      Serial.println("tmr_stop");  
+    }
+    
+    beep();    
+
+  }
+  return true;
+}
+
+void beep(){
+    digitalWrite(BUZZ, HIGH);
+    delay(500);
+    digitalWrite(BUZZ, LOW);
+}
+
+USE_EVENTUALLY_LOOP(mgr);
