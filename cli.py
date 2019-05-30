eb01fd0334dce9b3697db3c0c9146638b96a6fea
MarcelKaemper <marcelkaemper862@gmail.com>
2019-05-30 12:05:54 +0200

    Added time measuring

--- cli.py
+++ cli.py
@@ -1,3 +1,4 @@
+import time
 import serial
 import serial.tools.list_ports
 
@@ -7,11 +8,24 @@ for p in ports:
 
 s = serial.Serial("COM9")
 
+timest = ""
+timeend = ""
+
 while 1:
     try:
         serial = s.readline().decode("utf-8").replace("\n","")
         print(serial)
+        if serial == "tmr_start\r":
+            timest = time.time()
+        elif serial == "tmr_stop\r":
+            timeend = time.time()
+
+        if timest != "" and timeend != "":
+            break
+        
     except KeyboardInterrupt:
         break
+
+print("Time needed: ", round(timeend - timest, 4), " secs")
 
 s.close()
