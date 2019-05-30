4f01255d5be61b7e1fb5080a93191f46702c4ff5
MarcelKaemper <marcelkaemper862@gmail.com>
2019-05-30 14:30:45 +0200

    Added function to read times

--- gui.py
+++ gui.py
@@ -3,6 +3,19 @@ import serial
 import serial.tools.list_ports
 import tkinter
 import threading
+from tkinter import ttk
+
+def saveTime(timeCurr):
+    with open("times.data", "a") as w:
+        w.write(timeCurr+"\n")
+
+
+def readTime():
+    times = []
+    with open("times.data", "r") as r:
+        for line in r.readlines():
+            times.append(line)
+    return times
 
 
 def timer():
@@ -21,12 +34,23 @@ def timer():
             timeend = time.time()
 
         if timest != "" and timeend != "":
-            txt.insert(0, (str(round(timeend-timest, 4))))
+            timeCurr = str(round(timeend-timest,4))
+
+            txt.delete(0, tkinter.END)
+            txt.insert(0, timeCurr)
+
+            saveTime(timeCurr)
+
+            timeCurr = ""
+            timest = ""
+            timeend = ""
 
     s.close()
 
 
+
 root = tkinter.Tk()
+root.geometry("500x500")
 root.title("Cubetimer - GUI")
 
 frame = tkinter.Frame()
@@ -34,6 +58,13 @@ frame.pack()
 
 txt = tkinter.Entry(frame, bd=5, insertwidth=1, font=30)
 txt.pack()
+
+var = ""
+
+cbb = tkinter.Listbox(frame) 
+for item in readTime():
+    cbb.insert(tkinter.END, item)
+cbb.pack()
 
 
 t = threading.Thread(target=timer, daemon=True)
