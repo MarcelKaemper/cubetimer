1db9a322caa3c9c6d2c44355b1a0a9523baaa7d4
MarcelKaemper <marcelkaemper862@gmail.com>
2019-06-02 19:33:59 +0200

    Added average time calculation

--- gui.py
+++ gui.py
@@ -4,6 +4,13 @@ import serial.tools.list_ports
 import tkinter
 import threading
 
+def getAvg(n):
+    sum = 0
+    for i in range(0,n):
+        sum+=float(cbb.get(i).replace("\n", ""))
+    return round(sum/n,4)
+
+
 def saveTime(timeCurr):
     with open("times.data", "a") as w:
         w.write(timeCurr+"\n")
@@ -12,6 +19,8 @@ def loadTimes():
     cbb.delete(0, tkinter.END)
     for item in readTimes():
         cbb.insert(0, item)
+    avg_three["text"] = "Avg. 3: "+str(getAvg(3))
+    avg_five["text"] = "Avg. 5: "+str(getAvg(5))
 
 
 def readTimes():
@@ -24,7 +33,6 @@ def readTimes():
 def deleteItem():
     try:
         toDelete = cbb.get(cbb.curselection())
-        print(cbb.get(cbb.curselection()))
         with open("times.data", "r") as f:
             lines = f.readlines()
         with open("times.data", "w") as f:
@@ -44,7 +52,6 @@ def timer():
 
     while 1:
         ser = s.readline().decode("utf-8").replace("\n","")
-        print(ser)
 
         if ser == "tmr_start\r":
             timest = time.time()
@@ -81,6 +88,12 @@ frame.pack()
 
 txt = tkinter.Entry(frame, bd=5, insertwidth=1, font=30)
 txt.pack()
+
+avg_three = tkinter.Label(frame)
+avg_three.pack()
+
+avg_five = tkinter.Label(frame)
+avg_five.pack()
 
 dele = tkinter.Button(frame, text="delete", command=deleteItem)
 dele.pack()
